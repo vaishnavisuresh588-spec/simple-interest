9795f09b418685448d3be7bf51a9a8d6b5b3fcc4
vaishnavisuresh588-spec <vaishnavisuresh588@gmail.com>
2025-10-04 09:23:11 +0530

    Update simpleinterest.py

--- simpleinterest.py
+++ simpleinterest.py
@@ -1,1 +1,13 @@
+# Simple Interest Calculator
+
+# Taking inputs from the user
+principal = float(input("Enter the Principal amount: "))
+rate = float(input("Enter the Rate of Interest (%): "))
+time = float(input("Enter the Time (in years): "))
+
+# Calculating Simple Interest
+simple_interest = (principal * rate * time) / 100
+
+# Displaying the result
+print("The Simple Interest is:", simple_interest)
 
